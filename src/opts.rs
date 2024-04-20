26843356ce34722ce8b08120fca073e6b725934d
wangcong <wangcong001@sensorsdata.cn>
2024-04-20 20:37:09 +0800

    feat: add genpass & support output pass strength

--- src/opts.rs
+++ src/opts.rs
@@ -13,6 +13,8 @@ pub struct Opts {
 pub enum SubCommand {
     #[command(name = "csv", about = "Convert CSV to JSON")]
     Csv(CsvOpts),
+    #[command(name = "genpass", about = "Generate a random password")]
+    GenPass(GenPassOpts),
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -37,6 +39,24 @@ pub struct CsvOpts {
 
     #[arg(long, default_value_t = true)]
     pub header: bool,
+}
+
+#[derive(Parser, Debug)]
+pub struct GenPassOpts {
+    #[arg(short, long, default_value_t = 16)]
+    pub length: u8,
+
+    #[arg(long, default_value_t = true)]
+    pub upper: bool,
+
+    #[arg(long, default_value_t = true)]
+    pub lower: bool,
+
+    #[arg(long, default_value_t = true)]
+    pub number: bool,
+
+    #[arg(long, default_value_t = true)]
+    pub symbol: bool,
 }
 
 fn verify_input_file(filename: &str) -> Result<String, &'static str> {
---
+++ src/process/gen_pass.rs
@@ -0,0 +1,51 @@
+use rand::seq::SliceRandom;
+use zxcvbn::zxcvbn;
+
+const UPPER: &[u8] = b"ABCDEFGHJKLMNPQRSTUVWXYZ";
+const LOWER: &[u8] = b"abcdefghjkmnopqrstuvwxyz";
+const NUMBER: &[u8] = b"123456789";
+const SYMBOL: &[u8] = b"!@#$%^&*_";
+
+pub fn process_gen_pass(
+    length: u8,
+    upper: bool,
+    lower: bool,
+    number: bool,
+    symbol: bool,
+) -> anyhow::Result<()> {
+    let mut rng = rand::thread_rng();
+    let mut pass = Vec::new();
+    let mut chars = Vec::new();
+
+    if upper {
+        chars.extend_from_slice(UPPER);
+        pass.push(*UPPER.choose(&mut rng).expect("UPPER won't be empty"));
+    }
+    if lower {
+        chars.extend_from_slice(LOWER);
+        pass.push(*LOWER.choose(&mut rng).expect("LOWER won't be empty"));
+    }
+    if number {
+        chars.extend_from_slice(NUMBER);
+        pass.push(*NUMBER.choose(&mut rng).expect("NUMBER won't be empty"));
+    }
+    if symbol {
+        chars.extend_from_slice(SYMBOL);
+        pass.push(*SYMBOL.choose(&mut rng).expect("SYMBOL won't be empty"));
+    }
+
+    for _ in 0..(length - pass.len() as u8) {
+        let char = chars.choose(&mut rng).expect("charts won't be empty");
+        pass.push(*char);
+    }
+
+    pass.shuffle(&mut rng);
+
+    let password = String::from_utf8(pass)?;
+    println!("{}", password);
+
+    let estimate = zxcvbn(&password, &[])?;
+    eprintln!("Estimated strength: {}", estimate.score());
+
+    Ok(())
+}
