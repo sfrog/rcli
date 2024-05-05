a30714382c1cbd2b306851259624609b3872f83d
wangcong <wangcong001@sensorsdata.cn>
2024-05-05 23:15:26 +0800

    fix: http server 支持访问根目录, /src/lib.rs 拼接修复, 使用 handlebars 代替 HTML 拼接

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -5,6 +5,9 @@ use axum::{
     routing::get,
     Router,
 };
+use handlebars::{to_json, Handlebars};
+use serde::Serialize;
+use serde_json::Map;
 use std::{net::SocketAddr, path::PathBuf, sync::Arc};
 use tower_http::services::ServeDir;
 use tracing::{info, warn};
@@ -12,6 +15,12 @@ use tracing::{info, warn};
 #[derive(Debug)]
 struct HttpServeState {
     path: PathBuf,
+}
+
+#[derive(Serialize)]
+struct ServeFile {
+    name: String,
+    path: String,
 }
 
 pub async fn process_http_serve(path: PathBuf, prot: u16) -> Result<()> {
@@ -30,7 +39,8 @@ pub async fn process_http_serve(path: PathBuf, prot: u16) -> Result<()> {
     // axum router
     let router = Router::new()
         .nest_service("/tower", dir_service)
-        .route("/*path", get(file_handler))
+        .route("/*path", get(path_handler))
+        .route("/", get(root_handler))
         .with_state(Arc::new(state));
 
     let listener = tokio::net::TcpListener::bind(&addr).await?;
@@ -40,16 +50,27 @@ pub async fn process_http_serve(path: PathBuf, prot: u16) -> Result<()> {
     Ok(())
 }
 
-async fn file_handler(
+async fn root_handler(State(state): State<Arc<HttpServeState>>) -> (StatusCode, HeaderMap, String) {
+    file_handler(state, "").await
+}
+
+async fn path_handler(
     State(state): State<Arc<HttpServeState>>,
     Path(path): Path<String>,
 ) -> (StatusCode, HeaderMap, String) {
-    let full_path = state.path.join(path);
+    file_handler(state, &path).await
+}
+
+async fn file_handler(
+    state: Arc<HttpServeState>,
+    pathname: &str,
+) -> (StatusCode, HeaderMap, String) {
+    let full_path = state.path.join(pathname);
     info!("Serving file {:?}", &full_path);
     let mut header_map = HeaderMap::new();
 
     if full_path.exists() {
-        match read_from_path(full_path).await {
+        match read_from_path(full_path, pathname).await {
             Ok((content, content_type)) => {
                 header_map.insert(header::CONTENT_TYPE, content_type.parse().unwrap());
                 (StatusCode::OK, header_map, content)
@@ -72,33 +93,44 @@ async fn file_handler(
     }
 }
 
-async fn read_from_path(path: PathBuf) -> Result<(String, &'static str)> {
+async fn read_from_path(path: PathBuf, pathname: &str) -> Result<(String, &'static str)> {
     if path.is_file() {
         let content = tokio::fs::read_to_string(path).await?;
         info!("File served successfully, read {} bytes", content.len());
         Ok((content, "text/plain"))
     } else {
-        let content = read_dir_to_html(path)?;
+        let content = read_dir_to_html(path, pathname)?;
         info!("Directory served successfully");
         Ok((content, "text/html"))
     }
 }
 
-fn read_dir_to_html(path: PathBuf) -> Result<String> {
+fn read_dir_to_html(path: PathBuf, pathname: &str) -> Result<String> {
     let dir = std::fs::read_dir(path)?;
 
-    let mut content = String::new();
+    let mut files = Vec::new();
 
     for entry in dir {
         let name = entry?.file_name();
-        content.push_str(&format!(
-            "<li><a href='{}'>{}</a></li>",
-            name.to_str().unwrap(),
-            name.to_str().unwrap(),
-        ));
+        let pathname = if pathname.is_empty() {
+            "".to_owned()
+        } else {
+            format!("/{}", pathname)
+        };
+
+        files.push(ServeFile {
+            name: name.to_string_lossy().to_string(),
+            path: format!("{}/{}", pathname, name.to_string_lossy()),
+        });
     }
-    let html = format!("<html><body><ul>{}</ul></body></html>", content);
-    Ok(html)
+
+    let mut handlebars = Handlebars::new();
+    handlebars.register_template_file("index", "./assets/template.hbs")?;
+
+    let mut data = Map::new();
+    data.insert("files".to_string(), to_json(files));
+
+    Ok(handlebars.render("index", &data)?)
 }
 
 #[cfg(test)]
@@ -111,8 +143,8 @@ mod tests {
             path: PathBuf::from("."),
         });
 
-        let path = Path("Cargo.toml".to_string());
-        let (status, _, body) = file_handler(State(state), path).await;
+        let path = "Cargo.toml";
+        let (status, _, body) = file_handler(state, path).await;
 
         assert_eq!(status, StatusCode::OK);
         assert!(body.trim().starts_with("[package]"));
@@ -124,10 +156,12 @@ mod tests {
             path: PathBuf::from("."),
         });
 
-        let path = Path("fixtures".to_string());
-        let (status, _, body) = file_handler(State(state), path).await;
+        let path = "fixtures";
+        let (status, _, body) = file_handler(state, path).await;
 
         assert_eq!(status, StatusCode::OK);
-        assert!(body.trim().contains("<a href='jwt.key'>jwt.key</a>"));
+        assert!(body
+            .trim()
+            .contains("<a href='/fixtures/jwt.key'>jwt.key</a>"));
     }
 }
